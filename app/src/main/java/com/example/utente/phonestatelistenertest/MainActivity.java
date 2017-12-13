ee7ea2250cffe6ef06e74b2d0a26afe5e1a065f4
Salvatore Catania <salvatore.catania@live.it>
2017-12-13 12:54:23 +0100

    Initial commit

---
+++ app/src/main/java/com/example/utente/phonestatelistenertest/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.utente.phonestatelistenertest;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_PHONE_CALL = 1234;
+    Button buttonStartCall;
+    boolean permissionGranted = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        askPermission();
+
+        // add PhoneStateListener for monitoring
+        MyPhoneListener phoneListener = new MyPhoneListener();
+        TelephonyManager telephonyManager =
+                (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        // receive notifications of telephony state changes
+        telephonyManager.listen(phoneListener, PhoneStateListener.LISTEN_CALL_STATE);
+
+        buttonStartCall = (Button) findViewById(R.id.buttonStartCall);
+        buttonStartCall.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onClick(View v) {
+                String uri = "tel:+393489144050";
+                Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse(uri));
+                startActivity(callIntent);
+            }
+        });
+    }
+
+    public void askPermission(){
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_PHONE_CALL);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PHONE_CALL : {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    permissionGranted = true;
+                }else{
+                    permissionGranted = false;
+                }
+            }
+        }
+    }
+
+    private class MyPhoneListener extends PhoneStateListener {
+
+        private boolean onCall = false;
+
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+
+            switch (state) {
+                case TelephonyManager.CALL_STATE_RINGING:
+                    // phone ringing...
+                    Toast.makeText(MainActivity.this, incomingNumber + " calls you",
+                            Toast.LENGTH_LONG).show();
+                    break;
+
+                case TelephonyManager.CALL_STATE_OFFHOOK:
+                    // one call exists that is dialing, active, or on hold
+                    Toast.makeText(MainActivity.this, "on call...",
+                            Toast.LENGTH_LONG).show();
+                    //because user answers the incoming call
+                    onCall = true;
+                    break;
+
+                case TelephonyManager.CALL_STATE_IDLE:
+                    // in initialization of the class and at the end of phone call
+
+                    // detect flag from CALL_STATE_OFFHOOK
+                    if (onCall == true) {
+                        Toast.makeText(MainActivity.this, "restart app after call",
+                                Toast.LENGTH_LONG).show();
+
+                        // restart our application
+                        Intent restart = getBaseContext().getPackageManager().
+                                getLaunchIntentForPackage(getBaseContext().getPackageName());
+                        restart.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(restart);
+
+                        onCall = false;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+
+}
